ee9a3b24e6127217c78e04dde0a7420c9f442d57
Manikantan Narasimhan <rvns03@gmail.com>
2016-11-14 10:43:30 +0530

    Provide tab-completion of usernames in chat commands

--- client/shell-chat-client.py
+++ client/shell-chat-client.py
@@ -1,4 +1,5 @@
 import cmd
+import re
 import socket
 import sys
 import threading
@@ -23,12 +24,20 @@ class ChatClient(cmd.Cmd):
 	ruler = '-'
 
 
+	@staticmethod
+	def validUsername(name):
+		pattern = re.compile(r'[a-zA-Z0-9_]+')
+		match = pattern.match(name)
+		return (match is not None) and (match.group() == name)  
+
+
 	def __init__(self, port, username):
 		cmd.Cmd.__init__(self)
 		self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 		self.Socket.connect(('localhost',int(port)))
 		self.do_name(username, True)
 		self.setPrompt(username)
+		self.namesList = []
 
 
 	def setPrompt(self, name):
@@ -36,33 +45,46 @@ class ChatClient(cmd.Cmd):
 
 
 	def emptyline(self):
-		self.refresh()
+		self.updateMessages()
+		self.updateUsers()
 
 
 	def default(self, line):
 		self.stdout.write('ERROR : Unrecognised command "%s"\n'%line)
-		self.refresh()
+		self.updateMessages()
+		self.updateUsers()
 
 
 	def do_psend(self,line):
 		'Send message privately to an user. Syntax is "psend <username> <message>"'
-		self.refresh()
+		self.updateMessages()
+		self.updateUsers()
 		self.Socket.send('psend ' + line)
 		response = self.Socket.recv(1024)
 		print response
 
 
+	def complete_psend(self, text, *ignore):
+		return [name for name in self.namesList if name.startswith(text)]
+
+
 	def do_gsend(self,line):
 		'Send message to the chatroom. Syntax is "gsend <message>"'
-		self.refresh()
+		self.updateMessages()
+		self.updateUsers()
 		self.Socket.send('gsend ' + line)
 		response = self.Socket.recv(1024)
 		print response
 
 
+	def complete_gsend(self, text, *ignore):
+		return [name for name in self.namesList if name.startswith(text)]
+
+
 	def do_name(self,name,initial = False):
 		'Change your name. Only alphabets, digits and underscores are allowed in the name'
-		self.refresh()
+		self.updateMessages()
+		self.updateUsers()
 		name = name.split()[0]
 		self.Socket.send('name ' + name)
 		response = self.Socket.recv(1024)
@@ -75,28 +97,40 @@ class ChatClient(cmd.Cmd):
 			self.setPrompt(name)
 
 
-	def do_ol(self,line):
+	def do_ol(self,line,echo = True):
 		'Find users who are online'
-		self.refresh()
+		self.updateMessages()
 		self.Socket.send('ol')
 		response = self.Socket.recv(1024)
-		print response
+		if echo:
+			print response
+		words = response.split('\n')
+		self.namesList = filter(lambda word : ChatClient.validUsername(word), words)
 
 
 	def do_messages(self,line):
 		'View all the received messages'
-		self.refresh()
+		self.updateMessages()
+		self.updateUsers()
 		self.Socket.send('messages ' + line)
 		response = self.Socket.recv(1024)
 		print response
 
 
+	def complete_messages(self, text, *ignore):
+		return [name for name in self.namesList if name.startswith(text)]
+
+
 	def do_EOF(self, line):
-		self.refresh()
+		self.updateMessages()
 		return True
 
 
-	def refresh(self):
+	def updateUsers(self):
+		self.do_ol('', echo = False) # This will update list of online users
+
+
+	def updateMessages(self):
 		self.Socket.setblocking(False)
 		try:
 			data = self.Socket.recv(1024)
